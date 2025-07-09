ceb364c5e35cc3fe436a540443191f255db02726
tincan1596 <sahilatwal2003@gmail.com>
2025-07-09 20:54:40 +0000

    forced ether fixed

--- src/UnexpectedEther /good.sol
+++ src/UnexpectedEther /good.sol
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.30;
-// forcing ether to a contract
-// standard practice - invarient checking - dotn't rely on "address(this).balance".
-// self-destruc and prefund to the address are 2 possible ways to change contract balance without using payable function.
-// this contract allow 0.5 ether to be sent to it and reward 10 ether to the last player, any player can claim the existing ether balance before reaching 10 ether.
 
 contract EtherGame {
     uint256 public constant MinEther = 0.5 ether;
     uint256 public constant MaxEther = 10 ether;
-    uint256 public currentBalance = address(this).balance;
+    uint256 private currentBalance = 0;
 
     receive() external payable {
         deposit();
