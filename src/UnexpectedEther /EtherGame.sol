bdc63b6056deb2c5a82b0a0df547b1eacec56845
tincan1596 <sahilatwal2003@gmail.com>
2025-07-09 20:07:03 +0000

    forced ether

--- src/UnexpectedEther /EtherGame.sol
+++ src/UnexpectedEther /EtherGame.sol
@@ -1,2 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.30;
+// forcing ether to a contract
+// standard practice - invarient checking
+// it is a misconception that ether can be sent to a contract only through a payable function
