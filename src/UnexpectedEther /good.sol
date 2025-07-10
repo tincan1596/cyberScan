54993439b450103999e2aea90dd2ec1609f0c211
tincan1596 <sahilatwal2003@gmail.com>
2025-07-10 20:23:08 +0000

    clean ethergame

--- src/UnexpectedEther /good.sol
+++ src/UnexpectedEther /good.sol
@@ -2,30 +2,31 @@
 pragma solidity ^0.8.30;
 
 contract EtherGame {
-    uint256 public constant MinEther = 0.5 ether;
+    uint256 public constant stake = 0.5 ether;
     uint256 public constant MaxEther = 10 ether;
     uint256 private currentBalance = 0;
+    bool private locked;
 
     receive() external payable {
         deposit();
     }
 
-    function deposit() public payable {
-        require(msg.value >= MinEther, "0.5 ether only");
+    modifier noReentrancy() {
+        require(!locked, "No reentrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    function deposit() public payable noReentrancy {
+        require(msg.value == stake, "0.5 ether only");
         if (currentBalance < MaxEther) {
             currentBalance += msg.value;
         } else {
-            currentBalance = 0;
-            (bool success,) = msg.sender.call{value: 10}("");
-            require(success, "failed");
+            uint256 balance = currentBalance;
+            require(currentBalance >= MaxEther, "Not enough ether to reward");
+            (bool success,) = msg.sender.call{value: balance}("");
+            require(success, "Transfer failed");
         }
-    }
-
-    function withdraw() external {
-        require(currentBalance > 0, "No ether to withdraw");
-        uint256 amount = currentBalance;
-        currentBalance = 0;
-        (bool success,) = msg.sender.call{value: amount}("");
-        require(success, "failed");
     }
 }
