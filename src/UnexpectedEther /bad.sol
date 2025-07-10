1c08d7a228a2ed0a7b73cf0f25f002e95fd9b9f2
tincan1596 <sahilatwal2003@gmail.com>
2025-07-10 18:18:25 +0000

    adding units

--- src/UnexpectedEther /bad.sol
+++ src/UnexpectedEther /bad.sol
@@ -20,7 +20,7 @@ contract EtherGame {
             currentBalance += msg.value;
         } else {
             currentBalance = 0;
-            (bool success,) = msg.sender.call{value: 10 ether}("");
+            (bool success,) = msg.sender.call{value: MaxEther}("");
             require(success, "failed");
         }
     }
