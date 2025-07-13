6a9c3814e9d97c784b970327cc57beaab7ab1f33
tincan1596 <sahilatwal2003@gmail.com>
2025-07-13 20:22:30 +0000

    library and contract deployemtn

---
+++ src/DelegateCall/FibonacciLib_contract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract FibContract {
+    function fib(uint256 start, uint256 n) external pure returns (uint256) {
+        if (n == 0) return start;
+        if (n == 1) return start + 1;
+
+        uint256 a = start;
+        uint256 b = start + 1;
+
+        unchecked {
+            for (uint256 i = 2; i <= n; ++i) {
+                uint256 temp = a + b;
+                a = b;
+                b = temp;
+            }
+        }
+
+        return b;
+    }
+}
